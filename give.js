feb9b9b362b90ba9043d476cadf991767c6a7cdc
Aman <jhonman02@gmail.com>
2023-08-17 18:21:06 +0530

    seventh  commit

--- give.js
+++ give.js
@@ -111,177 +111,280 @@ if (window.innerWidth >= 768) {
       scrub: 2,
     },
   });
-}
-// image ends here
-
-gsap.from(".second .title", {
-  x: -500,
-  duration: 1,
-  scrollTrigger: {
-    scroller: ".main",
-    trigger: ".second .title",
-    start: "top 110%",
-    end: "bottom 10%",
-  },
-});
-gsap.from(".second .overview", {
-  x: 600,
-  duration: 1,
-  scrollTrigger: {
-    scroller: ".main",
-    trigger: ".second .overview",
-    start: "top 105%",
-    end: "bottom 10%",
-  },
-});
-// Challenge starts here
-
-gsap.from(".problem h1", {
-  y: 200,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".problem h1",
-    scroller: ".main",
-    start: "top 100%",
-    end: "bottom 10%",
-  },
-});
-gsap.from(".challenge .col-1", {
-  x: -500,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".challenge .col-1",
-    scroller: ".main",
-    start: "top 80%",
-    end: "bottom 10%",
-  },
-});
-gsap.from(".challenge .col-2", {
-  x: 800,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".challenge .col-1",
-    scroller: ".main",
-    start: "top 80%",
-    end: "bottom 10%",
-  },
-});
-gsap.from(".solution-title h1", {
-  y: 200,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".solution-title h1",
-    scroller: ".main",
-    start: "top 100%",
-    end: "bottom 10%",
-  },
-});
-gsap.from(".solution .sol-col-1", {
-  x: -1000,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".solution .sol-col-1",
-    scroller: ".main",
-    start: "top 80%",
-    end: "bottom 10%",
-  },
-});
-gsap.from(".solution .sol-col-2", {
-  x: 1000,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".solution .sol-col-1",
-    scroller: ".main",
-    start: "top 80%",
-    end: "bottom 10%",
-  },
-});
-
-// challange and sol ends here
-const tg = gsap.timeline();
-tg.from(".gal-1", {
-  x: -500,
-  // backgroundPositionX: "-70%",
-  duration: 1,
-  scrollTrigger: {
-    scroller: ".main",
-    trigger: ".gal-1",
-    start: "top 55%",
-    end: "top 0%",
-    scrub: 2,
-    // pin:true,
-    // markers:true
-  },
-});
-tg.from(
-  ".gal-2",
-  {
-    x: 500,
-    // backgroundPositionX: "70%",
-
-    duration: 0.6,
-
+  // image ends here
+  gsap.from(".second .title", {
+    x: -500,
+    duration: 1,
     scrollTrigger: {
       scroller: ".main",
-      trigger: ".gal-2",
+      trigger: ".second .title",
+      start: "top 110%",
+      end: "bottom 10%",
+    },
+  });
+
+  gsap.from(".second .overview", {
+    x: 600,
+    duration: 1,
+    scrollTrigger: {
+      scroller: ".main",
+      trigger: ".second .overview",
+      start: "top 105%",
+      end: "bottom 10%",
+    },
+  });
+  // Challenge starts here
+
+  gsap.from(".problem h1", {
+    y: 200,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".problem h1",
+      scroller: ".main",
+      start: "top 100%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".challenge .col-1", {
+    x: -500,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".challenge .col-1",
+      scroller: ".main",
+      start: "top 80%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".challenge .col-2", {
+    x: 800,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".challenge .col-1",
+      scroller: ".main",
+      start: "top 80%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".solution-title h1", {
+    y: 200,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".solution-title h1",
+      scroller: ".main",
+      start: "top 100%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".solution .sol-col-1", {
+    x: -1000,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".solution .sol-col-1",
+      scroller: ".main",
+      start: "top 80%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".solution .sol-col-2", {
+    x: 1000,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".solution .sol-col-1",
+      scroller: ".main",
+      start: "top 80%",
+      end: "bottom 10%",
+    },
+  });
+}
+// challange and sol ends here
+if (window.innerWidth >= 768) {
+  const tg = gsap.timeline();
+  tg.from(".gal-1", {
+    x: -500,
+    // backgroundPositionX: "-70%",
+    duration: 1,
+    scrollTrigger: {
+      scroller: ".main",
+      trigger: ".gal-1",
       start: "top 55%",
       end: "top 0%",
       scrub: 2,
+      // pin:true,
+      // markers:true
     },
-  },
-  "<50%"
-);
-
-const TechFunction = () => {
-  const t = gsap.timeline();
-
-  t.to(".tech-bar h3", { y: 27, ease: "power4.out", duration: 0.3 });
-  t.to(".st", {
-    y: 45,
-    x: -30,
-    stagger: 0.06,
-    ease: "power4.out",
-    duration: 0.3,
   });
-};
+  tg.from(
+    ".gal-2",
+    {
+      x: 500,
+      // backgroundPositionX: "70%",
 
-// Close tech stack
+      duration: 0.6,
 
-const closeTech = () => {
-  const t = gsap.timeline();
-  t.to(".st", {
-    y: -35,
-    stagger: 0.06,
-    ease: "power4.out",
-    duration: 0.3,
+      scrollTrigger: {
+        scroller: ".main",
+        trigger: ".gal-2",
+        start: "top 55%",
+        end: "top 0%",
+        scrub: 2,
+      },
+    },
+    "<50%"
+  );
+
+  const TechFunction = () => {
+    const t = gsap.timeline();
+
+    t.to(".tech-bar h3", { y: 27, ease: "power4.out", duration: 0.3 });
+    t.to(".st", {
+      y: 45,
+      x: -30,
+      stagger: 0.06,
+      ease: "power4.out",
+      duration: 0.3,
+    });
+  };
+
+  // Close tech stack
+
+  const closeTech = () => {
+    const t = gsap.timeline();
+    t.to(".st", {
+      y: -35,
+      stagger: 0.06,
+      ease: "power4.out",
+      duration: 0.3,
+    });
+    t.to(".tech-bar h3", { y: -5, ease: "power4.out", duration: 0.3 });
+  };
+
+  var cpy = document.querySelector(".logo-slide").cloneNode(true);
+  document.querySelector(".logos").appendChild(cpy);
+
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  // Format the time as "hh:mm"
+  const timeFormat = `${hours}:${minutes}`;
+
+  console.log("Current time:", timeFormat);
+
+  document.querySelector(".footer-left .date").innerHTML = `${timeFormat}  IST`;
+
+  gsap.from(".t", {
+    y: -100,
+    opacity: 0,
+    duration: 0.8,
+
+    scrollTrigger: {
+      scroller: ".main",
+      trigger: ".t",
+      start: "top 60%",
+      end: "botton 30%",
+      scrub: 2,
+    },
   });
-  t.to(".tech-bar h3", { y: -5, ease: "power4.out", duration: 0.3 });
-};
+}
+if (window.innerWidth <= 768) {
+  gsap.from(".second .title", {
+    opacity: 0,
+    x: -500,
+    duration: 1,
+    scrollTrigger: {
+      scroller: ".main",
+      trigger: ".second .title",
+      start: "top 50%",
+      end: "bottom 10%",
+    },
+  });
 
-var cpy = document.querySelector(".logo-slide").cloneNode(true);
-document.querySelector(".logos").appendChild(cpy);
+  gsap.from(".second .overview", {
+    x: 600,
+    duration: 1,
+    scrollTrigger: {
+      scroller: ".main",
+      trigger: ".second .overview",
+      start: "top 40%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".t", {
+    y: -100,
+    opacity: 0,
+    duration: 0.8,
 
-const date = new Date();
-const hours = String(date.getHours()).padStart(2, "0");
-const minutes = String(date.getMinutes()).padStart(2, "0");
+    scrollTrigger: {
+      scroller: ".main",
+      trigger: ".t",
+      start: "top 30%",
+      end: "botton 30%",
+      // scrub: 2,
+    },
+  });
 
-// Format the time as "hh:mm"
-const timeFormat = `${hours}:${minutes}`;
+  // Challenge starts here
 
-console.log("Current time:", timeFormat);
-
-document.querySelector(".footer-left .date").innerHTML = `${timeFormat}  IST`;
-
-gsap.from(".t", {
-  y: -100,
-  opacity: 0,
-  duration: 0.8,
-
-  scrollTrigger: {
-    scroller: ".main",
-    trigger: ".t",
-    start: "top 60%",
-    end: "botton 30%",
-    scrub: 2,
-  },
-});
+  gsap.from(".problem h1", {
+    opacity: 0,
+    y: 200,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".problem h1",
+      scroller: ".main",
+      start: "top 80%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".challenge .col-1", {
+    x: -500,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".challenge .col-1",
+      scroller: ".main",
+      start: "top 70%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".challenge .col-2", {
+    x: 800,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".challenge .col-1",
+      scroller: ".main",
+      start: "top 60%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".solution-title h1", {
+    opacity: 0,
+    y: 200,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".solution-title h1",
+      scroller: ".main",
+      start: "top 70%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".solution .sol-col-1", {
+    x: -1000,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".solution .sol-col-1",
+      scroller: ".main",
+      start: "top 60%",
+      end: "bottom 10%",
+    },
+  });
+  gsap.from(".solution .sol-col-2", {
+    x: 1000,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".solution .sol-col-1",
+      scroller: ".main",
+      start: "top 60%",
+      end: "bottom 10%",
+    },
+  });
+}
